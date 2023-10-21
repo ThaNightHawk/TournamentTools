86e1e74251f155ed53e10a082a9fcceed2ee9087
Patrick Sørensen <hasfeldt@yahoo.dk>
2023-10-21 14:27:35 +0200

    1V1 | Players now go to their correct elements

--- Frontends/Frontend - Local/1V1/js/OverlayHandlers.js
+++ Frontends/Frontend - Local/1V1/js/OverlayHandlers.js
@@ -1,7 +1,7 @@
 //Sets the overlay data for Player 1, Player 2 and the round.
 async function setOverlay(playerIDs, playerNames, Round) {
 	document.getElementById("roundTextP").innerText = Round;
-	fetch('https://skillsaber.vercel.app/api/player?id=' + playerIDs[0])
+	await fetch('https://skillsaber.vercel.app/api/player?id=' + playerIDs[0])
 		.then(response => response.json())
 		.then(data => {
 			let playerCountry = data.country;
@@ -12,16 +12,16 @@ async function setOverlay(playerIDs, playerNames, Round) {
 			document.getElementById("Player1Name").style.opacity = '1';
 			document.getElementById("Player1Rank").style.opacity = '0.6';
 		});
-	fetch('https://skillsaber.vercel.app/api/player?id=' + playerIDs[1])
+	await fetch('https://skillsaber.vercel.app/api/player?id=' + playerIDs[1])
 		.then(response => response.json())
 		.then(data => {
 			let playerCountry = data.country;
-			document.getElementById("Player1Image").src = data.profilePicture;
-			document.getElementById("Player1Name").innerText = playerNames[1];
-			document.getElementById("Player1Rank").innerText = '#' + data.rank + ' Global | #' + data.countryRank + ' ' + playerCountry;
-			document.getElementById("Player1Flag").src = "https://flagicons.lipis.dev/flags/4x3/" + playerCountry.toLowerCase() + ".svg";
-			document.getElementById("Player1Name").style.opacity = '1';
-			document.getElementById("Player1Rank").style.opacity = '0.6';
+			document.getElementById("Player2Image").src = data.profilePicture;
+			document.getElementById("Player2Name").innerText = playerNames[1];
+			document.getElementById("Player2Rank").innerText = '#' + data.rank + ' Global | #' + data.countryRank + ' ' + playerCountry;
+			document.getElementById("Player2Flag").src = "https://flagicons.lipis.dev/flags/4x3/" + playerCountry.toLowerCase() + ".svg";
+			document.getElementById("Player2Name").style.opacity = '1';
+			document.getElementById("Player2Rank").style.opacity = '0.6';
 		});
 
 	setTimeout(function () {
