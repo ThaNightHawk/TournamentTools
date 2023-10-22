91959474d83e84832558f325d521688a523372e9
Patrick Sørensen <hasfeldt@yahoo.dk>
2023-10-22 11:13:23 +0200

    Add missing helpers on local version

--- Frontends/Frontend - Local/WebPanel/assets/js/Helpers.js
+++ Frontends/Frontend - Local/WebPanel/assets/js/Helpers.js
@@ -1,3 +1,164 @@
+/* Notification Helpers */
+
+const createSwal = ({
+    title,
+    input,
+    inputPlaceholder,
+    heightAuto = true,
+    showCancelButton = false,
+    allowOutsideClick = false,
+    allowEscapeKey = false,
+    confirmButtonText,
+    footer,
+    inputValidator,
+}) => {
+    return Swal.fire({
+        title,
+        input,
+        inputPlaceholder,
+        heightAuto,
+        showCancelButton,
+        allowOutsideClick,
+        allowEscapeKey,
+        confirmButtonText,
+        footer,
+        inputValidator,
+    });
+};
+
+const matchNotifConf = {
+    plainText: false,
+    clickToClose: true,
+    timeout: 5000,
+    position: 'right-bottom'
+}
+
+const mapPoolNotifConf = {
+    title: 'Select a map pool',
+    input: 'select',
+    inputOptions: songOptions,
+    inputPlaceholder: 'Select a map pool',
+    showCancelButton: false,
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    inputValidator: (value) => {
+        return new Promise((resolve) => {
+            if (value) {
+                resolve();
+            } else {
+                resolve('You need to select something!');
+            }
+        })
+    }
+}
+
+const swalPBConfig = {
+    title: 'Pick or Ban?',
+    showDenyButton: true,
+    showDenyButton: true,
+    confirmButtonText: `Pick`,
+    denyButtonText: `Ban`,
+    cancelButtonText: `Back`,
+    confirmButtonColor: '#439555',
+    denyButtonColor: '#eb6868',
+    cancelButtonColor: '#464646',
+    showCancelButton: true,
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+};
+
+const swalConfig = {
+    heightAuto: true,
+    showCancelButton: false,
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    confirmButtonText: 'Confirm',
+    inputValidator: (value) => {
+        return new Promise((resolve) => {
+            if (value) {
+                resolve();
+            } else {
+                resolve('You need to input something!');
+            }
+        });
+    },
+};
+
+/* Diff-color and Diff-Label Helpers */
+function getDiffColor(diff) {
+    switch (diff) {
+        case "easy":
+            return "#008055";
+        case "normal":
+            return "#1268A1";
+        case "hard":
+            return "#BD5500";
+        case "expert":
+            return "#B52A1C";
+        case "expertplus":
+        case "expert+":
+            return "#454588";
+        default:
+            return "#000000";
+    }
+}
+
+function getDiffLabel(diff) {
+    switch (diff) {
+        case "easy":
+            return "Easy";
+        case "normal":
+            return "Normal";
+        case "hard":
+            return "Hard";
+        case "expert":
+            return "Expert";
+        case "expertplus":
+        case "expert+":
+            return "Expert+";
+        default:
+            return "";
+    }
+}
+
+/* VERSUS Helpers */
+const dropdown = document.querySelector('#currentMatch');
+let selectedMatch = dropdown.options[dropdown.selectedIndex];
+
+dropdown.addEventListener('change', function () {
+    selectedMatch = dropdown.options[dropdown.selectedIndex];
+});
+
+function groupPlayersByTeam(players) {
+    const teams = {};
+    players.forEach(player => {
+        const teamId = player.team[1];
+        if (!teams[teamId]) {
+            teams[teamId] = [player];
+        } else {
+            teams[teamId].push(player);
+        }
+    });
+    return teams;
+}
+
+function sortTeamsByName(teams) {
+    return teams.map(team => team.sort((a, b) => a.name.localeCompare(b.name)));
+}
+
+function getPlayerNames(team1, team2, players) {
+    const defaultPlayer = { name: "Placeholder", user_id: "76561198086326146", team: [null, null], guid: "00000000-0000-0000-0000-000000000000" };
+    let [player1, player2, player3, player4] = [defaultPlayer, defaultPlayer, defaultPlayer, defaultPlayer];
+
+    if (players.length === 2) {
+        [player1, player2] = [team1[0] || defaultPlayer, team1[1] || defaultPlayer];
+    } else if (players.length > 2) {
+        [player1, player2, player3, player4] = [team1[0] || defaultPlayer, team1[1] || defaultPlayer, team2[0] || defaultPlayer, team2[1] || defaultPlayer];
+    }
+
+    return [player1, player2, player3, player4];
+}
+
 function getOptionHtml(matchId, coordinator, player1, player2, player3, player4, players) {
     let optionHtml;
     if (players.length < 3) {
